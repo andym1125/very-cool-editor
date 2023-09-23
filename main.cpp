d11f6bef71e4e9248236238d5542d0f1fdf95d5a
Andy <37462713+andym1125@users.noreply.github.com>
2023-09-23 14:10:56 -0500

    End lesson checkpoint

--- main.cpp
+++ main.cpp
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include <ncurses.h>
 
+using namespace std;
+
 int main()
 {
 	initscr();
-    printf("Test");
+
+    FILE* file = fopen("text.txt", "rb");
+	const char filename[] = "text.txt";
+	if(file == NULL)
+	{
+		printf("Fatal error opening file %s", filename);
+		return -1;
+	}
+
+	const int BUFFER_SIZE = 256;
+	char buffer[BUFFER_SIZE];
+	int readchars = fread(buffer, sizeof(char), BUFFER_SIZE, file);
+	printw(buffer);
+	while(readchars == BUFFER_SIZE)
+	{
+		readchars = fread(buffer, sizeof(char), BUFFER_SIZE, file);
+		printw(buffer);
+	}
+    
+	getch();
+	fclose(file);
+    endwin();
+	printf("%d", readchars);
 }
